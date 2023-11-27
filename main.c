a358373f58415b9cdb1afbc9698050236cb79d7f
sh1okoh <kooooo.0719task@gmail.com>
2023-11-27 21:34:12 +0900

    feat: i32.load, store命令を追加

--- main.c
+++ main.c
@@ -2,15 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define GLOBAL_SET 0x23
+#define GLOBAL_GET 0x23
+#define GLOBAL_SET 0x24
 #define LOCAL_GET  0x20
 #define LOCAL_SET  0x21
 #define I32_CONST  0x41
+#define I32_STORE  0x36
 #define I32_SUB    0x6B
 
 unsigned char IMEM[1024]; /* instruction memory */
+unsigned char DMEM[1024]; /* data memory */
 unsigned int Stack[1024];
-unsigned int Globals[256];
+unsigned int Globals[256]={1024}; /* g0 = 1024 */
 unsigned int Locals[256][256];
 unsigned int StackPtr;
 unsigned int PC, DEPTH;
@@ -102,6 +105,53 @@ void i32_sub ( void )
   y = Stack[StackPtr--];
   Stack[++StackPtr] = y - x;
 
+#if 1
+  printf ( "sub = %d\n", Stack[StackPtr] );
+#endif
+
+  return;
+}
+
+void i32_load ( void )
+{
+  unsigned int ea, i, offset, align;
+
+  align  = IMEM[PC++];
+  offset = IMEM[PC++]; /* Caution: can be leb128 */
+
+  i = Stack[StackPtr--];
+  ea = i + offset;
+
+  Stack[++StackPtr] = ((unsigned int)DMEM[ea+3] << 24) | ((unsigned int)DMEM[ea+2] << 16) | ((unsigned int)DMEM[ea+1] << 8) | (unsigned int)DMEM[ea];
+
+#if 1
+  printf ( "Stack[%d](<=DMEM[%d]): \%8X\n", StackPtr, ea, Stack[StackPtr] );
+#endif
+
+  return;
+}
+
+
+void i32_store ( void )
+{
+  unsigned int ea, c, i, offset, align;
+
+  align  = IMEM[PC++];
+  offset = IMEM[PC++]; /* Caution: can be leb128 */
+
+  c = Stack[StackPtr--];
+  i = Stack[StackPtr--];
+  ea = i + offset;
+
+  DMEM[ea]   = (unsigned char)((c >> 0)  & 0x000000FF);
+  DMEM[ea+1] = (unsigned char)((c >> 8)  & 0xFF);
+  DMEM[ea+2] = (unsigned char)((c >> 16) & 0xFF);
+  DMEM[ea+3] = (unsigned char)((c >> 24) & 0xFF);
+
+#if 1
+  printf ( "MEM[%d]: \%2X, %2X, %2X, %2X\n", ea, DMEM[ea+3],DMEM[ea+2],DMEM[ea+1],DMEM[ea] );
+#endif
+
   return;
 }
 
@@ -115,9 +165,13 @@ void execute ( void )
   inst = IMEM[PC++];
 
   switch ( inst ) {
-  case GLOBAL_SET:
+  case GLOBAL_GET:
     printf ("global.get\n");
     global_get ( );
+    break;
+  case GLOBAL_SET:
+    printf ("global.set\n");
+    global_set ( );
     break;
   case LOCAL_GET:
     printf ("local.get\n");
@@ -130,6 +184,10 @@ void execute ( void )
   case I32_CONST:
     printf ("i32.const\n");
     i32_const ( );
+    break;
+  case I32_STORE:
+    printf ("i32.store\n");
+    i32_store ( );
     break;
   case I32_SUB:
     printf ("i32.sub\n");
